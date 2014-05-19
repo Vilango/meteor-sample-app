3c17bacbeed0763c46785d2a5360377dff732cbe
Gerwin Brunner <gerwin.brunner@gmail.com>
2014-05-19 15:21:45 +0200

    added iron-router stub

---
+++ tests/iron-router-stub.js
@@ -0,0 +1,25 @@
+var emptyFunction = function () {
+};
+
+Router = {
+    map: emptyFunction,
+    configure: emptyFunction,
+    current: emptyFunction,
+    onBeforeAction: emptyFunction
+};
+
+var RouteControllerClass = function(route){
+    //apply properties of route to controller so you can test against them later
+    for(var i in route){
+        //noinspection JSUnfilteredForInLoop
+        this[i] = route[i];
+    }
+};
+
+RouteControllerClass.prototype = {
+    extend: function(route) {
+        return new RouteControllerClass(route);
+    }
+};
+
+RouteController = new RouteControllerClass();
